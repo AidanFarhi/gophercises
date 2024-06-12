1ba23456cb66e450029d66faae8e407a16359a74
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-06-12 00:26:50 -0400

    added basic quiz functionality

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -8,17 +8,39 @@ import (
 
 const FILE_NAME = "problems.csv"
 
-func main() {
+func getLinesFromCSV(fileName string) ([][]string, error) {
 	file, err := os.Open(FILE_NAME)
+	defer file.Close()
 	if err != nil {
-		fmt.Println("error opening CSV file")
-		os.Exit(1)
+		return nil, err
 	}
 	reader := csv.NewReader(file)
 	lines, err := reader.ReadAll()
 	if err != nil {
-		fmt.Println("error reading lines")
+		return nil, err
+	}
+	return lines, err
+}
+
+func askQuestionAndGetResult(quizItemNumber int, quizItem []string) bool {
+	question, correctAnswer, userAnswer := quizItem[0], quizItem[1], ""
+	fmt.Printf("Question #%d: %s = ", quizItemNumber, question)
+	fmt.Scanln(&userAnswer)
+	return userAnswer == correctAnswer
+}
+
+func main() {
+	lines, err := getLinesFromCSV(FILE_NAME)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(lines)
+	score := 0
+	numQuestions := len(lines)
+	for i, quizItem := range lines {
+		if askQuestionAndGetResult(i+1, quizItem) {
+			score++
+		}
+	}
+	fmt.Printf("Results: %d/%d correct\n", score, numQuestions)
 }
